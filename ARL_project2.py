50d5f285d65cf731526dcb242c5ef3b97aaab447
alvarooki <59296913+alvarooki@users.noreply.github.com>
2022-08-17 00:05:49 +0200

    Add files via upload

---
+++ ARL_project2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **My name is Álvaro Rodríguez**  
+# I work as an experimental physicist
+
+# *I am interested in data science because it's consider as the future of experimental sciences*
+
+# ### Cell 5 code prints a message
+
+# In[1]:
+
+
+print("This is my first code in Python")
+
+
+# - List of web pages
+#     - Python: (https://www.python.org/)
+#     - Jupyter (https://jupyter.org/)
+#     - ~ Numpy ~
+#  
+
+# In[ ]:
+
+
+
+
